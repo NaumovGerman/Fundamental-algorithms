be65f1bc5b21bab53b506f0058d81040f9f4f0d3
NaumovGerman <90508172+NaumovGerman@users.noreply.github.com>
2022-10-12 21:03:35 +0300

    Update ex9_1.c

--- lab2/exer9/ex9_1.c
+++ lab2/exer9/ex9_1.c
@@ -23,7 +23,6 @@ char* convert_to_usual(char *word, int base) {
         } else if (isalpha(*ptr)) {
             temp = toupper(*ptr) - 'A' + 10;
         } else {
-            //free(word);
             free(new);
             return NULL; // или другую ошибку
         }
@@ -53,6 +52,11 @@ char* smart_sum(char* result, char* word, int base) {
     diff = size_1 - size_2;
     if (diff > 0) {
         first = (char*)malloc(sizeof(char) * (size_1 + 2)); // 0 в начали и \0 в конце
+        if (first == NULL) {
+            free(result);
+            free(word);
+            return NULL;
+        }
         first[0] = '0';
 
         char *temp_1 = first + 1;
@@ -66,6 +70,12 @@ char* smart_sum(char* result, char* word, int base) {
         printf("first in smart_sum - %s\n", first);
 
         second = (char*)malloc(sizeof(char) * (size_1 + 2));
+        if (second == NULL) {
+            free(first);
+            free(result);
+            free(word);
+            return NULL;
+        }
         char *temp_2 = second;
         for (int i = 0; i < diff + 1; i++) {
             *temp_2++ = '0';
@@ -79,10 +89,16 @@ char* smart_sum(char* result, char* word, int base) {
         }
         *ptr_2 = '\0';
 
-
         printf("second in smart_sum -%s\n", second);
 
         output = (char*)malloc(sizeof(char) * (size_1 + 2));
+        if (output == NULL) {
+            free(first);
+            free(second);
+            free(result);
+            free(word);
+            return NULL;
+        }
         ptr_1 = first + (size_1 + 1);
         *ptr_1-- = '\0';
         ptr_2 = second + (size_1 + 1);
@@ -92,6 +108,11 @@ char* smart_sum(char* result, char* word, int base) {
 
     } else {
         first = (char*)malloc(sizeof(char) * (size_2 + 2));
+        if (first == NULL) {
+            free(result);
+            free(word);
+            return NULL;
+        }
         first[0] = '0';
 
         char *temp_1 = first + 1;
@@ -102,14 +123,20 @@ char* smart_sum(char* result, char* word, int base) {
             ptr_2++;
         }
         *ptr_2 = '\0';
+
         printf("first in smart_sum - %s\n", first);
-        //printf("%s\n", first);
 
         second = (char*)malloc(sizeof(char) * (size_2 + 2));
+        if (second == NULL) {
+            free(first);
+            free(result);
+            free(word);
+            return NULL;
+        }
         char *temp_2 = second;
         for (int i = 0; i < abs(diff) + 1; i++) {
             *temp_2++ = '0';
-        //     // temp_2++;
+
         }
         ptr_1 = result;
         while (*ptr_1) {
@@ -119,9 +146,16 @@ char* smart_sum(char* result, char* word, int base) {
         }
         *ptr_1 = '\0';
         printf("second in smart_sum -%s\n", second);
-        //printf("%s\n", second);
+
 
         output = (char*)malloc(sizeof(char) * (size_2 + 2));
+        if (output == NULL) {
+            free(first);
+            free(second);
+            free(result);
+            free(word);
+            return NULL;
+        }
         ptr_1 = first + (size_2 + 1);
         *ptr_1-- = '\0';
         ptr_2 = second + (size_2 + 1);
@@ -137,10 +171,6 @@ char* smart_sum(char* result, char* word, int base) {
         sic += isdigit(*ptr_2) ? *ptr_2 - '0' : toupper(*ptr_2) - 'A' + 10;
         sic += ost;
         printf("sic with ost- %d ", sic);
-        ////// *ptr_3 = sic>base ? sic%base - 10 + 'A': sic%base + '0';
-
-        ////// temp = sic%base;
-        ////// *ptr_3 = sic%base > 9;
         temp = sic%base;
         if (temp < 10) {
             *ptr_3 = temp + '0';
@@ -157,10 +187,13 @@ char* smart_sum(char* result, char* word, int base) {
     printf("\n");
     free(first);
     free(second);
-    // printf("%c\n", output[0]);
+
     if (output[0] == '0') {
-        printf("here?1\n");
         go = (char*)malloc(sizeof(char)*co);
+        if (go == NULL) {
+            free(output);
+            return NULL;
+        }
         ptr_1 = output + 1;
         ptr_2 = go;
         while (*ptr_1){
@@ -169,16 +202,11 @@ char* smart_sum(char* result, char* word, int base) {
             ptr_2++;
         }
         *ptr_2 = '\0';
-        // printf("here?\n");
         free(output);
         return go;
     } else {
         return output;
     }
-    
-    //go = convert_to_usual(output, base);
-    //free(output);
-    // printf("%s\n", go);
     return go;
 }
 
@@ -201,41 +229,41 @@ char* find_sum(int count, int base, ...) {
             free(result);
             return NULL;
         }
-        // printf("a in find_sum - %s\n", a);
-        // printf("%d\n", size);
         printf("result = %s\n", result);
         printf("a = %s\n", a);
         act = smart_sum(result, a, base);
-        // printf("%s\n", act);
-        //// if (act == NULL) {
-        ////     free(result);
-        ////     free(a);
-        ////     return NULL;
-        //// }
+        if (act == NULL) {
+            free(result);
+            free(a);
+            return NULL;
+        }
         size = strlen(act);
         result = (char*)malloc(sizeof(char)*(size + 1));
+        if (result == NULL) {
+            free(act);
+            return NULL;
+        }
         strcpy(result, act);
         printf("result_1 = %s\n", result);
         printf("\n");
         free(act);
     }
     va_end(runner);
-    // free(act);
     return result;
 }
 
 int main() {
-    int base = 2;
-    // char* hey = find_sum(6, (int) base, (char*) "12345", (char*) "12", (char*) "123", (char*) "1234", (char*) "12345", (char*) "123456"); // 149515 base-10
+    int base = 10;
+    char* hey = find_sum(6, (int) base, (char*) "12345", (char*) "12", (char*) "123", (char*) "1234", (char*) "12345", (char*) "123456"); // 149515 base-10
     // char* hey = find_sum(2, (int) base, (char*) "FFFF", (char*) "A7681B37FF8ECC");
     // char* hey = find_sum(5, (int) base, (char*) "7939", (char*) "16B52", (char*) "57955", (char*) "D94D5", (char*) "61285");
     // char* hey = find_sum(2, (int) base, (char*) "7939FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", (char*) "16B52");
-    char* hey = find_sum(2, (int) base, (char*) "01010101001", (char*) "111110101");
+    // char* hey = find_sum(2, (int) base, (char*) "01010101001", (char*) "111110101");
     // char* hey = find_sum(2, (int) base, (char*) "75DE0", (char*) "D94D5");
     if (hey == NULL) {
         printf("oh..\n");
     }else {
-        // printf("%s\n", hey);
+        printf("%s\n", hey);
         free(hey);
     }
     return 0;
